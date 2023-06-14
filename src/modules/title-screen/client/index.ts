b3836e25ef4c347401c7a8f34ccbb22e9847eb18
turupawn <ahmed.hn.43@gmail.com>
2023-06-14 17:52:34 -0600

    removed uneeded libs

--- src/modules/title-screen/client/index.ts
+++ src/modules/title-screen/client/index.ts
@@ -1,13 +1,11 @@
 import { RpgClient, RpgModule, RpgGui } from '@rpgjs/client'
 import titleGui from './gui/title.vue'
-import loginGui from './gui/connect.vue'
 import { sprite } from './sprite'
 
 @RpgModule<RpgClient>({ 
     sprite,
     gui: [
-        titleGui,
-        loginGui
+        titleGui
     ]
 })
 export default class RpgClientEngine {}
--- src/modules/title-screen/server/index.ts
+++ src/modules/title-screen/server/index.ts
@@ -4,16 +4,6 @@ import { RpgServer, RpgModule, RpgPlayer } from '@rpgjs/server'
     player: {
         onConnected(player: RpgPlayer) {
             const gui = player.gui('rpg-title-screen')
-            gui.on('load-game', ({ index }) => {
-                const storage = localStorage.getItem('rpgjs-save')
-                if (storage) {
-                    const slots = JSON.parse(storage)
-                    const slot = slots[index]
-                    gui.close()
-                    player.load(slot)
-                    player.canMove = true
-                }
-            })
             gui.on('start-game', () => {
                 gui.close()
                 player.changeMap('simplemap')
